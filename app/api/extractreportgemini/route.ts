4260ab970e54c22e84011f49c9e944216334b6e9
anasyakubu <yakubuanas04@gmail.com>
2024-11-17 23:14:56 +0100

    changes

--- app/api/extractreportgemini/route.ts
+++ app/api/extractreportgemini/route.ts
@@ -1,33 +1,35 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+  model: "gemini-1.5-pro",
 });
 
-const prompt = `Attached is an image of a clinical report. 
-Go over the the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date etc. Make sure to include numerical values and key details from the report, including report title.
+const prompt = `Attached is an image of a study material.  
+Review the study material and identify key concepts or topics that are particularly significant or complex. Then summarize the material in 100 words. You may increase the word limit if the material spans multiple pages.  
+Avoid including unnecessary details like author names, dates, or unrelated information. Ensure your summary highlights key points, numerical data (if relevant), and essential details, including the title of the study material.  
 ## Summary: `;
 
 export async function POST(req: Request, res: Response) {
-    const { base64 } = await req.json();
-    const filePart = fileToGenerativePart(base64)
+  const { base64 } = await req.json();
+  const filePart = fileToGenerativePart(base64);
 
-    console.log(filePart);
-    const generatedContent = await model.generateContent([prompt, filePart]);
+  console.log(filePart);
+  const generatedContent = await model.generateContent([prompt, filePart]);
 
-    console.log(generatedContent);
-    const textResponse = generatedContent.response.candidates![0].content.parts[0].text;
-    return new Response(textResponse, { status: 200 })
+  console.log(generatedContent);
+  const textResponse =
+    generatedContent.response.candidates![0].content.parts[0].text;
+  return new Response(textResponse, { status: 200 });
 }
 
 function fileToGenerativePart(imageData: string) {
-    return {
-        inlineData: {
-            data: imageData.split(",")[1],
-            mimeType: imageData.substring(
-                imageData.indexOf(":") + 1,
-                imageData.lastIndexOf(";")
-            ),
-        },
-    }
+  return {
+    inlineData: {
+      data: imageData.split(",")[1],
+      mimeType: imageData.substring(
+        imageData.indexOf(":") + 1,
+        imageData.lastIndexOf(";")
+      ),
+    },
+  };
 }
