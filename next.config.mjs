877d4dc7865e30b7221edf51cc83161a3789349f
anasyakubu <yakubuanas04@gmail.com>
2024-11-20 05:57:56 +0100

    chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
   // (Optional) Export as a standalone site
   // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
   output: "standalone", // Feel free to modify/remove this option
