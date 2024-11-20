d5855e2a4dbad8b68a5313b017d57bc95a768adb
anasyakubu <yakubuanas04@gmail.com>
2024-11-20 08:15:58 +0100

    fixed errors

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import markdownit from "markdown-it";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 
 type Props = {
   text: string;
 };
 
+// Initialize Markdown-It
 const md = markdownit({
+  html: true, // Allow HTML tags in Markdown
+  linkify: true, // Automatically link URLs
+  typographer: true, // Enable smart quotes, dashes, etc.
 });
 
 const Markdown = ({ text }: Props) => {
-  const htmlcontent = md.render(text);
-  const sanitized = DOMPurify.sanitize(htmlcontent);
-  return <div dangerouslySetInnerHTML={{ __html: sanitized }}></div>;
+  // Render Markdown to HTML
+  const htmlContent = md.render(text);
+
+  // Check for DOM availability (important for SSR)
+  const sanitizedContent =
+    typeof window !== "undefined"
+      ? DOMPurify.sanitize(htmlContent)
+      : htmlContent; // If SSR, skip sanitization
+
+  return (
+    <div
+      className="markdown"
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+    />
+  );
 };
 
 export default Markdown;
