8147d7a7be1f4d991327bddf05d93e4b1281420d
anasyakubu <yakubuanas04@gmail.com>
2024-11-20 17:06:55 +0100

    fixed error

--- app/lingua-speak/page.tsx
+++ app/lingua-speak/page.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import Nav from "@/components/Nav/Nav";
-import LinguaSpeakContent from "../../components/LinguaSpeakContent/linguaSpeakContent";
+import LinguaSpeakContent from "../../components/LinguaSpeakContent/LinguaSpeakContent";
 import React from "react";
 
 const linguaspeak: React.FC = () => {
